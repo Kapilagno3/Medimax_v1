d11a465e9b68ed0bf172691bf52ef3dbe0454298
Kapil Agnihotri <kapilagnihotri202@gmail.com>
2025-11-22 22:59:55 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="generative ai project",
+    version="0.0.0",
+    author="Kapil Agnihotri",
+    author_email="kapilagnihotri101@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
